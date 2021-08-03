69cb2a901fd16bdceee70f125cc0df8a43ef1a2a
rafael garcia <rago89@gmail.com>
2021-08-03 08:42:56 +0200

    fix conflict isolate/1-event-loop/exercises/exercise-1.js

--- isolate/1-event-loop/exercises/exercise-1.js
+++ isolate/1-event-loop/exercises/exercise-1.js
@@ -1,42 +1,42 @@
-import { labeledLogger } from '../../../lib/labeled-logger.js';
+import { labeledLogger } from "../../../lib/labeled-logger.js";
 
 const log = labeledLogger(Date.now());
 
 // fill in the blanks
 
-let x = '';
+let x = "";
 
-x += 'java';
+x += "ja";
 
 const callback1 = () => {
-  x += 'ri';
-  log('cb 1:', x);
+  x += "ri";
+  log("cb 1:", x);
 };
 setTimeout(callback1, 1000);
 
-x += '';
+x += "va";
 
 const callback2 = () => {
-  const test = x === 'javascript';
-  log('cb 2:', test);
+  const test = x === "javascript";
+  log("cb 2:", test);
   console.assert(test, 'x should be "javascript"');
 };
 setTimeout(callback2, 2000);
 
 const callback3 = () => {
-  x += 'sc';
-  log('cb 3:', x);
+  x += "sc";
+  log("cb 3:", x);
 };
 setTimeout(callback3, 500);
 
 const callback4 = () => {
-  x += 'pt';
-  log('cb 4:', x);
+  x += "pt";
+  log("cb 4:", x);
 };
 setTimeout(callback4, 1500);
 
-x += '';
+x += "";
 
 log(x);
 
-log('= = = =  the call stack is empty  = = = =');
+log("= = = =  the call stack is empty  = = = =");
